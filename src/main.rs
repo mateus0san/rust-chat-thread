5bb8b3f84fb62952e5718d70deccd6f41b78fdbb
Mateus S. Araujo <mateus0santiago@proton.me>
2026-02-20 23:42:16 -0300

    New version, from the ground again

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,113 @@
-use std::{collections::HashMap, sync::Arc, thread};
-
-use chat::{Client, Server};
-
-#[derive(Default)]
-struct Room {
-    clients: HashMap<usize, Client>,
-    capacity: usize,
-}
-
-impl Room {
-    fn new(capacity: usize) -> Self {
-        Self {
-            clients: HashMap::new(),
-            capacity,
-        }
-    }
-
-    fn is_full(&self) -> bool {
-        self.clients.len() == self.capacity
-    }
-}
+use std::{
+    collections::HashMap,
+    io::{self, BufRead, BufReader, Read, Write},
+    net::{SocketAddr, TcpListener, TcpStream},
+    sync::mpsc,
+    thread,
+};
 
 fn main() {
-    let (server, receiver) = Server::bind_server();
-    let state_server = Arc::clone(&server.connection);
+    let listener = TcpListener::bind("0.0.0.0:1337").expect("ERROR: could not start the server");
+    let (sender, receiver) = mpsc::channel::<Message>();
 
-    thread::spawn(move || {
-        server.run();
+    thread::spawn(|| {
+        let mut clients = HashMap::new();
+        for msg in receiver {
+            match msg {
+                Message::Broadcast(msg) => new_message(msg, &mut clients),
+                Message::NewClient(client) => {
+                    if let Some(new_client) = new_client(&mut clients, client) {
+                        eprintln!("INFO: New client {}", new_client.ip);
+                    } else {
+                        eprintln!("INFO: Ip address of new client is already on the server.")
+                    }
+                }
+            }
+        }
     });
 
-    let mut room = Room::new(42);
+    for stream in listener.incoming() {
+        let sender = sender.clone();
+        match stream {
+            Err(e) => eprintln!("INFO: new stream returned an error {e}"),
+            Ok(stream) => {
+                thread::spawn(|| match handle_connection(stream, sender) {
+                    Err(e) => eprintln!("INFO: connection failed: {e}"),
+                    Ok(e) => eprintln!("INFO: {e}"),
+                });
+            }
+        }
+    }
+}
 
-    for mut client in receiver {
-        if room.is_full() {}
+fn new_client(clients: &mut HashMap<SocketAddr, Client>, client: Client) -> Option<&mut Client> {
+    if clients.get(&client.ip).is_some() {
+        client
+            .writer
+            .shutdown(std::net::Shutdown::Both)
+            .expect("This stream must to shutdown");
+        return None;
+    }
+
+    Some(clients.entry(client.ip).or_insert(client))
+}
+
+fn new_message(msg: String, clients: &mut HashMap<SocketAddr, Client>) {
+    let msg = msg.as_bytes();
+    let _removed_clients: HashMap<SocketAddr, Client> = clients
+        .extract_if(|_k, v| v.writer.write_all(msg).is_err())
+        .collect();
+}
+
+enum Message {
+    Broadcast(String),
+    NewClient(Client),
+}
+
+struct Client {
+    ip: SocketAddr,
+    writer: TcpStream,
+}
+
+impl Client {
+    fn try_new(stream: TcpStream) -> Result<Client, io::Error> {
+        let ip = stream.peer_addr()?;
+
+        Ok(Client::new(stream, ip))
+    }
+
+    fn new(writer: TcpStream, ip: SocketAddr) -> Self {
+        Self { ip, writer }
+    }
+}
+
+fn handle_connection(
+    stream: TcpStream,
+    sender: mpsc::Sender<Message>,
+) -> Result<&'static str, io::Error> {
+    let recv_msg = "receiver dropped";
+    let end_normal = "connection closed normally";
+
+    let reader = stream.try_clone()?;
+
+    let client = Client::try_new(stream)?;
+    if sender.send(Message::NewClient(client)).is_err() {
+        return Ok(recv_msg);
+    }
+
+    let mut reader = BufReader::new(reader);
+
+    loop {
+        let mut msg = String::with_capacity(120);
+
+        match reader.by_ref().take(128).read_line(&mut msg) {
+            Ok(0) => break Ok(end_normal),
+            Err(e) => break Err(e),
+            _ => {
+                if sender.send(Message::Broadcast(msg)).is_err() {
+                    break Ok(recv_msg);
+                }
+            }
+        }
     }
 }
